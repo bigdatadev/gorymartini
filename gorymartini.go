70586686e33d428c859130fb90cdb249b8100e6f
Christopher Gilbert <chris.gilbert@datasift.com>
2014-07-01 12:37:14 +0100

    Some bug fixes to gorymartini

--- gorymartini.go
+++ gorymartini.go
@@ -4,6 +4,7 @@
 package gorymartini
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -14,31 +15,31 @@ import (
 )
 
 // NewGoryMartini - Factory
-func NewGoryMartini(host string) (*GorymanClient, martini.Handler) {
+func NewGoryMartini(host string) (*goryman.GorymanClient, martini.Handler) {
 	riemann := goryman.NewGorymanClient(host)
 	err := riemann.Connect()
 	if err != nil {
 		return nil, nil
 	}
 
-	return riemann, func(res http.ResponseWriter, req *http.Request, c Context, log *log.Logger) {
+	return riemann, func(res http.ResponseWriter, req *http.Request, c martini.Context, log *log.Logger) {
 		start := time.Now()
 
-		rw := res.(ResponseWriter)
+		rw := res.(martini.ResponseWriter)
 		c.Next()
 
-		metric := float64(time.Since(start)) / float64(time.Milliseconds)
+		metric := float64(time.Since(start))
 
 		err := riemann.SendEvent(&goryman.Event{
 			Service:     "http req",
 			Metric:      metric,
-			Description: fmt.Printf("Request took %f seconds.", metric),
+			Description: fmt.Sprintf("Request took %f seconds.", metric),
 			Tags: []string{
 				"http",
 			},
-			Attributes: map[string]interface{}{
+			Attributes: map[string]string{
 				"path":   req.URL.Path,
-				"status": strconf.Itoa(rw.Status()),
+				"status": strconv.Itoa(rw.Status()),
 			},
 		})
 		if err != nil {
